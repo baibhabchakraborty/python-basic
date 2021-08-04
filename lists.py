b98002a1a26286a5a690a6a2abc1111d3ef7190c
Baibhab Chakraborty <baibhab.baishakhi@gmail.com>
2021-08-05 00:23:39 +0530

    basic stuff

---
+++ lists.py
@@ -0,0 +1,5 @@
+nums = {2,6,7,8,9,12,2}
+names = ['baibhab','ABC','!@#']
+
+#print(nums.index(6))
+print(nums)
---
+++ practice.py
@@ -0,0 +1,15 @@
+a = 4
+print(a)
+
+print('baibhab\'s laptop')
+print('Baibhab\nChakraborty')
+print(r'Baibhab\nChakraborty')
+
+name = 'baibhab'
+print(name)
+
+
+x = len(name)
+print(x)
+
+print(len(name))
